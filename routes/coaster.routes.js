a3bee0abea14fd0db740575c81b26662da094e0a
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-22 10:38:50 +0200

    create GET route to /coasters/new

--- routes/coaster.routes.js
+++ routes/coaster.routes.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+const Coaster = require('./../models/coaster.model')
+
 // Aquí los endpoints
 
+router.get('/new', (req, res) => res.render('coasters/new-coaster'))
 
 module.exports = router
