5bca60001f5ac5ba523cb12131be2501c3f11486
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-22 11:43:30 +0200

    /coasters now shows all the coasters in the db with their park names

--- routes/coaster.routes.js
+++ routes/coaster.routes.js
@@ -6,6 +6,8 @@ const Park = require('./../models/park.model')
 
 // Aquí los endpoints
 
+//CREATING COASTERS
+
 router.get('/new', (req, res) =>
   Park.find()
     .then((parks) => {
@@ -18,14 +20,27 @@ router.get('/new', (req, res) =>
 
 router.post('/new', (req, res, next) => {
   const { name, description, inversions, length, park } = req.body
-  console.log("hey")
+  console.log('hey')
 
-  Coaster.create({name, description, inversions, length, park})
+  Coaster.create({ name, description, inversions, length, park })
     .then(() => {
       res.redirect('/')
     })
     .catch((err) => {
       console.log(err)
+      next(err)
+    })
+})
+
+//COASTER INDEX + DETAILS
+
+router.get('/', (req, res) => {
+  Coaster.find()
+    .populate('park')
+    .then((coasters) => {
+      res.render('coasters/coasters-index', { coasters })
+    })
+    .catch((err) => {
       next(err)
     })
 })
