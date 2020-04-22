fbe989023280989ced851017c3847003616f7f9b
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-22 10:51:11 +0200

    form dropdown now has all parks on db as options

--- routes/coaster.routes.js
+++ routes/coaster.routes.js
@@ -2,9 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 const Coaster = require('./../models/coaster.model')
+const Park = require('./../models/park.model')
 
 // Aquí los endpoints
 
-router.get('/new', (req, res) => res.render('coasters/new-coaster'))
+router.get('/new', (req, res) =>
+  Park.find()
+    .then((parks) => {
+      res.render('coasters/new-coaster', { parks })
+    })
+    .catch((err) => {
+      next(err)
+    })
+)
 
 module.exports = router
